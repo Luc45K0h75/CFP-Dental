c9318d107e1a072a108f5cb05f48c134cbd318a9
Lucas Koh <lucasfkoh@gmail.com>
2026-06-11 18:31:07 +0800

    Added function that gets the current time

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 
 # Create the Flask application
 def create_app():
-    app = Flask(__name__) 
-    
+    app = Flask(__name__, instance_relative_config=True)
+
     from .routes import main
     app.register_blueprint(main)
+
+    @app.context_processor
+    def inject_now():
+        return {'now': datetime.utcnow()}
 
     return app
