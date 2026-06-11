ae5f596b3b241852ea046cf9929a8bb9e8fc5462
Lucas Koh <lucasfkoh@gmail.com>
2026-06-11 18:30:25 +0800

    Added backend routes

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/services')
+def services():
+    return render_template('services.html') 
+
+@main.route('/team')
+def team():
+    return render_template('team.html')
+
+@main.route('/location')
+def location():
+    return render_template('location.html')
+
+@main.route('/book')
+def book():
+    return render_template('book.html')
