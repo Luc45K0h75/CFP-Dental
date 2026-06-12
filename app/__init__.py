6a0630ec05a77d35b82ad053fcfcc3eb3808b22c
Lucas Koh <lucasfkoh@gmail.com>
2026-06-12 17:03:34 +0800

    Initialise mail parameters in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_mail import Mail
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+mail = Mail()
 
 # Create the Flask application
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    app.secret_key = os.getenv('SECRET_KEY', 'dev')
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+    mail.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
